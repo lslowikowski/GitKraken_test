65498f15a369c8db28ae77da84a046e219f968b0
Leszek <lslowikowski@gmail.com>
2020-04-22 13:45:53 +0200

    Dodanie obsługi przycisków

--- js/skrypt.js
+++ js/skrypt.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-    $("#informacja").click(function() {
+    $("#informacja").click(function () {
         alert("GitKraken to przyjazny program!");
+    });
+    
+    $("#repo").click(function () {
+        $('#tresc').load('repo.html');
+    });
+
+    $("#settings").click(function () {
+        $('#tresc').load('settings.html');
     });
 });
